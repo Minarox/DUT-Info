5658d8c2c9e4e519ea63dea98b99944e27b830b2
Minarox <mathis.sema@gmail.com>
2021-03-22 21:20:24 +0100

    Add files from GitLab

---
+++ pages/PHP/verification_93dwq2r85x.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Exercice 10</title>
+	</head>
+
+	<body>
+		<?php
+			//Connexion au serveur MySQL
+            require('db.php');
+			
+			$requete = 'SELECT * FROM Usager';
+			if(!$resquery = mysqli_query($link, $requete)) {
+				die("Erreur : ".mysqli_errno($link)." : ".mysqli_error($link));
+			}
+			else {
+				while($row = mysqli_fetch_array($resquery, MYSQL_NUM)) {
+					echo("Prénom : ".$row[0].", Nom : ".$row[1].", Adresse : ".$row[2].", CP : ".$row[3].", Ville : ".$row[4].", Téléphone : ".$row[5].'<br/>');
+				}
+			}
+		?>
+	</body>
+</html>
+<?php
+echo "<mm:dwdrfml documentRoot=" . __FILE__ .">";$included_files = get_included_files();foreach ($included_files as $filename) { echo "<mm:IncludeFile path=" . $filename . " />"; } echo "</mm:dwdrfml>";
+?>
