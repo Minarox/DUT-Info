7eb0c182b543aebc1b93f21ff37aed2233861b77
Minarox <contact@minarox.fr>
2024-06-05 14:48:43 +0000

    Clear Cabinet-medical files

--- pages/PHP/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try {
-        $linkpdo = new PDO("mysql:host={host};dbname={database}", "{username}", "{password}");
-    }
-    catch (Exception $e) {
-        die("Erreur : ".$e->getMessage());
-    }
-?>
